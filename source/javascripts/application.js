f3333700aa21b9a2a39ef171b0788f5d86f3e3e9
Justin Cook <justinrcook@gmail.com>
2013-12-13 15:59:33 -0600

    Removed unused js.

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,13 +1,2 @@
 //= require bootstrap/
 //= require_tree .
-
-/*
-// http://stackoverflow.com/questions/9288482/how-do-i-set-the-offset-for-scrollspy-in-bootstrap
-var offset = 90;
-
-$('nav li a').click(function(event) {
-  event.preventDefault();
-  $($(this).attr('href'))[0].scrollIntoView();
-  scrollBy(0, -offset);
-});
-*/
