e0c7dbbaa86287ad4b6de39d0e0b702ef65a9616
Justin Cook <justinrcook@gmail.com>
2013-12-13 16:04:00 -0600

    Collapse the navbar after clicking a link in mobile. Since it's a single page app the dropdown menu would stay open after clicking a link.

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,2 +1,9 @@
 //= require bootstrap/
 //= require_tree .
+
+$(document).ready(function () {
+  $("nav li a").click(function(event) {
+    // check if window is small enough so dropdown is created
+    $(".navbar-collapse").removeClass("in").addClass("collapse");
+  });
+});
