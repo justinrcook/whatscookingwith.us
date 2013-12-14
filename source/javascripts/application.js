7645dc636d4e8d7726c6be63797a3356ae0804c3
Justin Cook <justinrcook@gmail.com>
2013-12-13 21:19:52 -0600

    Using js to set the scroll spy offset and animating the transition down to the anchors farther down the page.

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -2,8 +2,32 @@
 //= require_tree .
 
 $(document).ready(function () {
+  // Collapse navbar after clicking on a link in mobile.
   $("nav li a").click(function(event) {
     // check if window is small enough so dropdown is created
     $(".navbar-collapse").removeClass("in").addClass("collapse");
+  });
+
+
+  //---------------------------------------
+  // Offset scrollspy with fixed navbar
+  // - http://stackoverflow.com/questions/17879846/boostrap-scrollspy-offset-on-a-fixed-navbar-does-not-work
+  // Height based on the height of the navbar
+  var offsetHeight = 81;
+
+  $('body').scrollspy({
+    offset: offsetHeight
+  });
+
+  $('nav li a').click(function(event) {
+    var scrollPos = $('body > .container').find($(this).attr('href')).offset().top - (offsetHeight - 1);
+
+    $('body,html').animate({
+      scrollTop: scrollPos
+    }, 500, function () {
+      $(".btn-navbar").click();
+    });
+
+    return false;
   });
 });
