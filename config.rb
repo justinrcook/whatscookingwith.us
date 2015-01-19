3217bd3f07efe84e72dfcfc09a6f312deb44d9d2
Justin Cook <justinrcook@gmail.com>
2015-01-18 22:41:57 -0800

    Initial middleman project.

---
+++ config.rb
@@ -0,0 +1,29 @@
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+  activate :relative_assets
+  activate :imageoptim      # Compress images
+end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true
+end
