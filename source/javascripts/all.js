50f5b73d9b653fe011aa92cc587cbd7d9d01a426
Justin Cook <justinrcook@gmail.com>
2015-03-22 19:42:34 -0700

    Only requiring the javascript we need.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,8 @@
-//= require 'bootstrap'
-//= require_tree .
+// Only require the bootstrap javascript we need,
+// see all avilable options in bootstrap-sprockets.js
+//= require "bootstrap/affix"
+//= require "bootstrap/button"
+//= require "bootstrap/transition"
 
 // Affix for sticky nav
 // Keep the sticky nav from jumping: http://stackoverflow.com/a/13151016
