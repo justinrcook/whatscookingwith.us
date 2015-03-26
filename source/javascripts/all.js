e2d98bf57f4acae52c58275cad10c98bde337e36
Justin Cook <justinrcook@gmail.com>
2015-03-25 20:55:45 -0700

    Reverted back to original sticky nav affix settings and html.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -8,11 +8,11 @@
 // Keep the sticky nav from jumping: http://stackoverflow.com/a/13151016
 
 $(function() {
-  $(".nav-wrapper").height($("nav").height());
 
   $("nav").affix({
     offset: {
-      top: $("nav").offset().top
+      top: $("header").height()
     }
   });
+
 });
