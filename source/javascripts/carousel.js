fe142727a64c70c5479b274d7b22b571cb85e002
Justin Cook <justinrcook@gmail.com>
2015-09-14 23:49:44 -0700

    Setup slick carousel js.

---
+++ source/javascripts/carousel.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		infinite: true,
+  	slidesToShow: 3,
+  	slidesToScroll: 3,
+	  responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 3,
+	        infinite: true,
+	        dots: true
+	      }
+	    },
+	    {
+	      breakpoint: 650,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+		]
+
+  });
+});
